55cd39dc850dd5235cd41548b644402929cfdcf2
20171105114 <13948948183@163. com>
2018-07-08 15:54:45 +0800

    text070802
    text070802

--- text070802/main.cpp
+++ text070802/main.cpp
@@ -5,11 +5,28 @@
 //  Created by lishuxin on 2018/7/8.
 //  Copyright © 2018年 lishuxin. All rights reserved.
 //
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char * argv[])
+{
+    FILE*a;
+    char s1[200];
+    char s2[200];
+    char s3[200];
+    char s4[200];
+    char s5[200];
+    char s6[200];
+    char s7[200];
+    if((a=fopen("/User/s20171105111/Desktop/test/File.csv","r"))==0)
+    {
+        printf("文件不存在、n");
+    }
+    else
+    {
+        fscanf(a,"%s ,%s ,%s ,%s ,%s ,%s ,%s",s1,s2,s3,s4,s5,s6,s7);
+        printf("%s\n,%s\n,%s\n,%s\n,%s\n,%s\n,%s\n",s1,s2,s3,s4,s5,s6,s7);
+        
+    }
     return 0;
+    
 }
